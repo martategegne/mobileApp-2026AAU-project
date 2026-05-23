21af080aafadc13ff864973b273d38e8522a7b2c
Ebise <tekle2ebise@gmail.com>
2026-05-23 08:15:07 -0700

    updated main

--- smart_resources/lib/main.dart
+++ smart_resources/lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
-import '../core/routing/app_router.dart';
-import '../core/theme/app_theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'core/routing/app_router.dart';
+import 'core/theme/app_theme.dart';
+import 'core/settings/settings_provider.dart';
 
 void main() {
-  runApp(const SmartStudyApp());
+  runApp(const ProviderScope(child: SmartStudyApp()));
 }
 
-class SmartStudyApp extends StatelessWidget {
+class SmartStudyApp extends ConsumerWidget {
   const SmartStudyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final settings = ref.watch(settingsProvider);
+    
     return MaterialApp.router(
       title: 'Smart Study',
-      theme: AppTheme.theme,
+      theme: AppTheme.theme(settings.isDarkMode, settings.fontSizeFactor),
       routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: false,
     );
